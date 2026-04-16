5a977bc1e72142d02e47002682d8fac6c4456d11
Jatin <jatinpratapsingh2002@gmail.com>
2026-04-16 14:55:32 +0530

    [Jatin Pratap Singh] Add . UC17 Google Authentication

--- src/main/java/com/quantity_measurement_app/repository/QuantityMeasurementRepository.java
+++ src/main/java/com/quantity_measurement_app/repository/QuantityMeasurementRepository.java
@@ -1,5 +1,4 @@
 package com.quantity_measurement_app.repository;
-
 import com.quantity_measurement_app.model.QuantityMeasurementEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
